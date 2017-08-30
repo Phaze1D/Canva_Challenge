a8905fbd93dc92b206e34753140c8a614db50948
DavidMBP <steadypathapp@gmail.com>
2017-08-30 00:42:43 -0500

    Added the uploader to the mosica file

---
+++ js/mosaic.js
@@ -0,0 +1,13 @@
+var exports = exports || null
+
+let TILE_WIDTH = 16
+let TILE_HEIGHT = 16
+
+if (exports) {
+  exports.TILE_WIDTH = TILE_WIDTH
+  exports.TILE_HEIGHT = TILE_HEIGHT  
+}else{
+  var uploader = new Uploader()
+}
+
+
